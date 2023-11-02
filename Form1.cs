5e1895068ad50cd196fcbf98b1d3cfb9c3218433
Ландсберг-Дым Иван <whiterex31@gmail.com>
2023-11-02 21:54:43 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,147 @@
+namespace KPP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Создадим необходимые переменные для моделирования
+        private static int t1; // Человек пришел на предприятие
+        private static int t2; // Человек ушел с предприятия
+        private static int T; // Человека проверяют
+        private static int Delta1; // Человек без документов, только уши, лапы и хвост
+        private static int Delta2; // Человек принес что-то запрещенное (УК РФ 228-232)
+        private static int dt; // Охранник уводит человека в комнату и делает с ним ВСЯКОЕ
+        private static int N; // Размер стека охранника
+
+        // Флаги моделирования
+        private static bool IsRunning = false; // Проверка на работу моделирования
+        private static bool IsChecking = false; // Правда - охранник занят и не может проверять, ложь - охранник свободен
+
+        // По работе программы
+        private static int TimeSpeed; // Для тракбара
+        private static int MinutesInDay; // Количество минут в дне
+        private static int Days; // Количество дней моделирования
+        private static int deltaT; // Шаг времени
+        private static string[] Protocol; // Для протоколирования
+        private static int QueueIn; // Людей в очереди на вход
+        private static int QueueOut; // Людей в очереди на выход
+        private static int FacesCount; // Общее количество обработанных людей
+        private static int Time; // Общее модельное время
+
+        // Интересные статистические данные
+        private static int NoDocs; // Людей без документов
+        private static int WithDrugs; // Людей со странным содержимым сумок
+        private static int PunishedPeoples; // Наказанных людей
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "2";
+            textBox2.Text = "3";
+            textBox3.Text = "5";
+            textBox4.Text = "10";
+            textBox5.Text = "5";
+            textBox6.Text = "15";
+            textBox7.Text = "20";
+            textBox8.Text = "1";
+            textBox9.Text = "1";
+        }
+
+        async private void main()
+        {
+            // Исходные данные стохастической модели
+            t1 = Convert.ToInt32(textBox1.Text); // Новый гость
+            t2 = Convert.ToInt32(textBox2.Text); // Кто-то уходит
+            T = Convert.ToInt32(textBox3.Text); // Проверка человека
+            Delta1 = Convert.ToInt32(textBox4.Text); // Человек без документов
+            Delta2 = Convert.ToInt32(textBox5.Text); // Что-то принес или ворует
+            dt = Convert.ToInt32(textBox6.Text); // Наказание человека по времени
+            N = Convert.ToInt32(textBox7.Text); // Размер стека охранника 
+            Days = Convert.ToInt32(textBox8.Text);
+
+            // 
+            int localTime1 = 0; // Время прихода человека
+            int localTime2 = 0; // Время выхода человека
+            // 
+            TimeSpeed = Convert.ToInt32(trackBar1.Value); // Скорость моделирования
+            MinutesInDay = Days * 24 * 60; // Количество минут в днях
+            int StepsCounter = 0; // Счетчик шагов моделирования
+            
+            for (Time = 0; Time <= MinutesInDay; Time += deltaT) // Общее модельное время
+            {
+                Protocol = new string[5] { "", "", "", "", "" };
+                StepsCounter += 1; // Количество шагов цикла
+                if (Time - localTime1 >= 0)
+                {
+                    // Значит человек пришел на КПП, запуск активности 1
+                    QueueIn += 1;
+                }
+
+                if (Time - localTime2 >= 0)
+                {
+                    // Значит человек уходит с КПП, запуск активности 2
+                    QueueOut += 1;
+                }
+                if ((QueueIn > 0 || QueueOut > 0) && IsChecking == false) // В какой-то из очередей есть люди и охранник готов их проверять
+                {
+                    // Значит охранник начинает проверку очереди, в которой больше человек, активность 3
+                    if (QueueIn > QueueOut)
+                    {
+                        QueueIn -= 1; // Человек заходит в рубку охранника
+                        Random random = new Random();
+                        int rnd = random.Next(0, 100); 
+                        if (rnd > Delta1) // Значит человек без документов
+                        {
+                            NoDocs += 1; 
+                        }
+                        if (rnd > Delta2) // Значит человек принес/вынес что-либо
+                        {
+                            WithDrugs += 1;
+                        }
+                    }
+                    else if (QueueOut > QueueIn)
+                    {
+                        QueueOut -= 1;
+                        Random random = new Random();
+                        int rnd = random.Next(0, 100);
+                        if (rnd > Delta2) 
+                        {
+
+                        }
+                    }
+                    else
+                    {
+                        // Если людей одинаково в обеих очередях, охранник уходит на перекур
+                    }
+
+                    // Если человек слишком много
+                    if (QueueIn + QueueOut >= N)
+                    {
+                        // Тогда охранник выпускает нужное количество людей из очереди на выход, не проверяя их, с запасом на 1 человека\
+
+                        int Kicked = Math.Abs(N - QueueIn - QueueOut) + 1;
+                        QueueOut -= Kicked;
+                        FacesCount += Kicked;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!IsRunning)
+            {
+                IsRunning = true;
+                button1.Text = "Стоп";
+                main();
+            }
+            else
+            {
+                IsRunning = false;
+                button1.Text = "Поехали!";
+            }
+        }
+    }
+}
